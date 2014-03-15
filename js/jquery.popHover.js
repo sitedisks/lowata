ab8a2486fbcdb275c87b590d6cf8835fe46bf731
sitedisks <peter@sitedisks.com.au>
2014-03-16 00:02:48 +1100

    More options / settings pass
    Add options and colours setting

--- js/jquery.popHover.js
+++ js/jquery.popHover.js
@@ -1,13 +1,11 @@
 (function($, window, document, undefind){
 	// local global go here - referenced via closure
-
+	var oSize;
+	
 	$.fn.turnGreen =function(options){
 		options = $.extend({}, $.fn.config, options);
-		
 		return this.each(function(){
-		
 			$(this).css('color', 'green');
-		 	
 		});
 	};
 
@@ -15,7 +13,27 @@
 		options = $.extend({}, $.fn.config, options);
 		
 		return this.each(function() {
+			oSize = parseInt($(this).css("font-size"));
+			$(this).mouseover(function() {
+				$(this).stop().animate({
+					opacity: options.opa
+					, fontSize: oSize * options.size
+				 
+				}, options.enlSpeed, function() {
+					$(this).animate({ 
+						opacity: 1
+						
+						, color: '#000000'
+					}, options.backSpeed);
+					
+				});
+			});
 			
+			$(this).mouseout(function() {
+				$(this).stop().animate({
+					fontSize: oSize
+				}, options.outSpeed);
+			});
 		});
 	};
 
@@ -26,6 +44,6 @@
 		, weight: 1.06
 		, enlSpeed: 400
 		, backSpeed: 400
-		, outSpeed: 100
+		, outSpeed: 400
 	};
 }(jQuery, window, document));
